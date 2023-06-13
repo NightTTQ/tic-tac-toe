b16602b55912acd68d76f57712697b268efb26f3
TTQ <ttq@ponder.fun>
2023-06-13 18:27:43 +0800

    修复了eslint报错的问题

--- src/components/board/handlers.ts
+++ src/components/board/handlers.ts
@@ -1,3 +1,75 @@
+/**
+ * @desc 生成连子游戏handler
+ * @param winCount 获胜所需连子数
+ * @param firstChess 先手棋子
+ * @param secondChess 后手棋子
+ */
+const universalHandler =
+    (winCount: number, firstChess: string, secondChess: string) =>
+        (
+            data: string[][],
+            newRow: number,
+            newCol: number,
+            column: number,
+            row: number,
+            chess: string,
+            step: number
+        ) => {
+        // 下棋
+            const newChess = data[newRow][newCol];
+            const returns = { nextChess: '', newWinner: '' };
+            let isWin = false;
+            const dir = [
+                [
+                    [1, 0],
+                    [-1, 0],
+                ],
+                [
+                    [0, 1],
+                    [0, -1],
+                ],
+                [
+                    [1, 1],
+                    [-1, -1],
+                ],
+                [
+                    [1, -1],
+                    [-1, 1],
+                ],
+            ];
+            for (const dr of dir) {
+                let count = 1;
+                for (const ddr of dr) {
+                    let xIndex = newCol + ddr[0];
+                    let yIndex = newRow + ddr[1];
+                    while (
+                        xIndex >= 0 &&
+                    xIndex < column &&
+                    yIndex >= 0 &&
+                    yIndex < row &&
+                    data[yIndex][xIndex] === newChess
+                    ) {
+                        xIndex += ddr[0];
+                        yIndex += ddr[1];
+                        count++;
+                        if (count >= winCount) {
+                            isWin = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            // 判断胜者
+            if (isWin) returns.newWinner = chess;
+            else {
+            // 判断平局
+                if (step === (column * row) - 1) returns.newWinner = '平局';
+                // 更新下一颗棋子
+                if (chess === firstChess) returns.nextChess = secondChess;
+                else if (chess === secondChess) returns.nextChess = firstChess;
+            }
+            return returns;
+        };
 const handlers: {
     name: string;
     handler: (
@@ -14,146 +86,14 @@ const handlers: {
     {
         // 三连子游戏
         name: 'tictactoe',
-        handler: (
-            data: string[][],
-            newRow: number,
-            newCol: number,
-            column: number,
-            row: number,
-            chess: string,
-            step: number
-        ) => {
-            // 下棋
-            const newChess = data[newRow][newCol];
-            const returns = { nextChess: '', newWinner: '' };
-            let isWin = false;
-            const dir = [
-                [
-                    [1, 0],
-                    [-1, 0],
-                ],
-                [
-                    [0, 1],
-                    [0, -1],
-                ],
-                [
-                    [1, 1],
-                    [-1, -1],
-                ],
-                [
-                    [1, -1],
-                    [-1, 1],
-                ],
-            ];
-            for (const d of dir) {
-                let count = 1;
-                for (const dd of d) {
-                    let x = newCol + dd[0];
-                    let y = newRow + dd[1];
-                    while (x >= 0 && x < column && y >= 0 && y < row && data[y][x] === newChess) {
-                        x += dd[0];
-                        y += dd[1];
-                        count++;
-                        if (count >= 3) {
-                            isWin = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            // 判断胜者
-            if (isWin) {
-                returns.newWinner = chess;
-            } else {
-                // 判断平局
-                if (step === column * row - 1) {
-                    returns.newWinner = '平局';
-                }
-                // 更新下一颗棋子
-                if (chess === 'X') {
-                    returns.nextChess = 'O';
-                } else if (chess === 'O') {
-                    returns.nextChess = 'X';
-                }
-            }
-            return returns;
-        },
-        init: () => {
-            return { nextChess: 'X' };
-        },
+        handler: universalHandler(3, 'X', 'O'),
+        init: () => ({ nextChess: 'X' }),
     },
     {
         // 五子棋游戏
         name: 'gomoku',
-        handler: (
-            data: string[][],
-            newRow: number,
-            newCol: number,
-            column: number,
-            row: number,
-            chess: string,
-            setp: number
-        ) => {
-            // 下棋
-            const newChess = data[newRow][newCol];
-            const returns = { nextChess: '', newWinner: '' };
-            let isWin = false;
-            const dir = [
-                [
-                    [1, 0],
-                    [-1, 0],
-                ],
-                [
-                    [0, 1],
-                    [0, -1],
-                ],
-                [
-                    [1, 1],
-                    [-1, -1],
-                ],
-                [
-                    [1, -1],
-                    [-1, 1],
-                ],
-            ];
-            for (const d of dir) {
-                let count = 1;
-                for (const dd of d) {
-                    let x = newCol + dd[0];
-                    let y = newRow + dd[1];
-                    while (x >= 0 && x < column && y >= 0 && y < row && data[y][x] === newChess) {
-                        x += dd[0];
-                        y += dd[1];
-                        count++;
-                        if (count >= 5) {
-                            isWin = true;
-                            break;
-                        }
-                    }
-                    if (isWin) break;
-                }
-                if (isWin) break;
-            }
-            // 判断胜者
-            if (isWin) {
-                returns.newWinner = chess;
-            } else {
-                // 判断平局
-                if (setp === column * row - 1) {
-                    returns.newWinner = '平局';
-                }
-                // 更新下一颗棋子
-                if (chess === '⚫') {
-                    returns.nextChess = '⚪';
-                } else if (chess === '⚪') {
-                    returns.nextChess = '⚫';
-                }
-            }
-            return returns;
-        },
-        init: () => {
-            return { nextChess: '⚫' };
-        },
+        handler: universalHandler(5, '⚫', '⚪'),
+        init: () => ({ nextChess: '⚫' }),
     },
 ];
 
